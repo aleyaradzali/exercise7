cba572aeacbc027ec546892c7c6b1d837c9d11dc
Aizadtul Aleya <aizadtulaleya@gmail.com>
2021-03-19 14:06:28 +0800

    exercise 7 commit

---
+++ Lab1/src/exercise7/Calculate.java
@@ -0,0 +1,25 @@
+package exercise7;
+import java.util.Scanner;
+
+
+public class Calculate {
+
+	public static void main(String[] args) {
+		
+		double weight;
+		KiraBerat berat = new KiraBerat();
+		Scanner print = new Scanner(System.in);
+		
+		System.out.println("<< Difference between human weight on Earth and on Mars. >>");
+		
+		System.out.println("\nPlease enter your weight: (kg)"); 
+		weight = print.nextDouble(); 
+		berat.setBerat(weight); 
+		
+		weight = berat.getBerat();
+		System.out.println("\n" + weight);
+	}
+	
+	
+
+}
---
+++ Lab1/src/exercise7/KiraBerat.java
@@ -0,0 +1,14 @@
+package exercise7;
+
+public class KiraBerat {
+	private double var;
+
+	public double getBerat() {
+		return var;
+	}
+
+	public void setBerat(double var) {
+		this.var = var;
+	}
+	
+}
